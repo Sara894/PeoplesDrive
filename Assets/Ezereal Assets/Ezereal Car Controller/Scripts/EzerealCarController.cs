ca1b05e591483b140bff22e221bcf077afd8a791
Sara Mitrevska <saramitrevska894@gmail.com>
2025-12-25 20:16:11 +0100

    changed car speed and map design

--- Assets/Ezereal Assets/Ezereal Car Controller/Scripts/EzerealCarController.cs
+++ Assets/Ezereal Assets/Ezereal Car Controller/Scripts/EzerealCarController.cs
@@ -20,7 +20,7 @@ namespace Ezereal
         public Transform rearRightWheelMesh;
 
         [Header("Settings")]
-        public float maxMotorTorque = 200f;
+        public float maxMotorTorque = 800f;
         public float maxSteerAngle = 5f;
         public float brakeTorque = 2000f;
         public float maxSpeed = 50f;
@@ -77,8 +77,17 @@ namespace Ezereal
             if (Mathf.Abs(motorInput) > 0.01f && speed < maxSpeed)
             {
                 // High torque at low speed, low torque at high speed
-                float accelFactor = Mathf.Lerp(1.5f, 0.1f, speed / maxSpeed);
+                //float accelFactor = Mathf.Lerp(1.5f, 0.1f, speed / maxSpeed);
+                float speed01 = speed / maxSpeed;
+                float accelFactor = Mathf.Lerp(2.5f, 0.8f, speed01);
+
                 motor = maxMotorTorque * motorInput * accelFactor;
+
+                if (speed < 5f && motorInput > 0.1f)
+                {
+                    motor *= 1.5f;
+                }
+
             }
             frontLeftWheelCollider.motorTorque = motor;
             frontRightWheelCollider.motorTorque = motor;
@@ -94,9 +103,14 @@ namespace Ezereal
             {
                 appliedBrake = brakeTorque;
             }
-            else if (Mathf.Approximately(motorInput, 0f))
+            //else if (Mathf.Approximately(motorInput, 0f))
+            //{
+            //    appliedBrake = brakeTorque;
+            //}
+
+            else
             {
-                appliedBrake = brakeTorque;
+                appliedBrake = 0f;
             }
             frontLeftWheelCollider.brakeTorque = appliedBrake;
             frontRightWheelCollider.brakeTorque = appliedBrake;
