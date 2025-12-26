28c2267af690b92c57536169c7862345f32392ca
Sara Mitrevska <saramitrevska894@gmail.com>
2025-12-26 20:32:23 +0100

    UI changes and map changes

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -14,31 +14,13 @@ public class PauseManager : MonoBehaviour
 
     [Header("Input Actions")]
     [SerializeField] private InputActionReference pauseAction;
-    [SerializeField] private InputActionReference navigateAction;
-    [SerializeField] private InputActionReference submitAction;
 
-    private Button[] buttons;
-    private int currentIndex = 0;
     private bool isPaused;
-
-    private void Awake()
-    {
-        buttons = new Button[]
-        {
-            resumeButton,
-            mainMenuButton
-        };
-    }
 
     private void OnEnable()
     {
         pauseAction.action.Enable();
-        navigateAction.action.Enable();
-        submitAction.action.Enable();
-
         pauseAction.action.performed += OnPause;
-        navigateAction.action.performed += OnNavigate;
-        submitAction.action.performed += OnSubmit;
 
         resumeButton.onClick.AddListener(ResumeGame);
         mainMenuButton.onClick.AddListener(BackToMainMenu);
@@ -48,12 +30,7 @@ public class PauseManager : MonoBehaviour
     private void OnDisable()
     {
         pauseAction.action.performed -= OnPause;
-        navigateAction.action.performed -= OnNavigate;
-        submitAction.action.performed -= OnSubmit;
-
         pauseAction.action.Disable();
-        navigateAction.action.Disable();
-        submitAction.action.Disable();
 
         resumeButton.onClick.RemoveListener(ResumeGame);
         mainMenuButton.onClick.RemoveListener(BackToMainMenu);
@@ -75,8 +52,8 @@ public class PauseManager : MonoBehaviour
         pauseCanvas.SetActive(true);
         isPaused = true;
 
-        currentIndex = 0;
-        SelectButton(currentIndex);
+        // Set the first selected button so keyboard/controller navigation works
+        EventSystem.current.SetSelectedGameObject(resumeButton.gameObject);
     }
 
     public void ResumeGame()
@@ -87,39 +64,6 @@ public class PauseManager : MonoBehaviour
         isPaused = false;
 
         EventSystem.current.SetSelectedGameObject(null);
-    }
-
-    private void OnNavigate(InputAction.CallbackContext ctx)
-    {
-        if (!isPaused)
-            return;
-
-        float y = ctx.ReadValue<Vector2>().y;
-
-        if (Mathf.Abs(y) < 0.5f)
-            return;
-
-        if (y < 0)
-            currentIndex++;
-        else
-            currentIndex--;
-
-        currentIndex = Mathf.Clamp(currentIndex, 0, buttons.Length - 1);
-
-        SelectButton(currentIndex);
-    }
-
-    private void OnSubmit(InputAction.CallbackContext ctx)
-    {
-        if (!isPaused)
-            return;
-
-        buttons[currentIndex].onClick.Invoke();
-    }
-
-    private void SelectButton(int index)
-    {
-        EventSystem.current.SetSelectedGameObject(buttons[index].gameObject);
     }
 
     public void BackToMainMenu()
